6000feebe66b0d63cbe4e47e633e8cd9409387ab
Zoey-Ang <20046523@myrp.edu.sg>
2021-05-03 10:55:13 +0800

    update

--- app/src/main/java/sg/edu/rp/c346/id20046523/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046523/demosimpleclick/MainActivity.java
@@ -3,12 +3,33 @@ package sg.edu.rp.c346.id20046523.demosimpleclick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnClick;
+    EditText etInput;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnClick = findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userInput = etInput.getText().toString();
+                tvDisplay.setText(userInput);
+            }
+
+        }
+        );
     }
 }
